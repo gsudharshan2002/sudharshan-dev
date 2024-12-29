fb7948f1d58b5b2352fec5f116321883f539da41
gsudharshan2002 <140174934+gsudharshan2002@users.noreply.github.com>
2024-12-30 00:42:21 +0530

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,52 @@ import Experience from "./components/Experiences.jsx";
 import Tech from "./components/Tech.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
+import { motion, useSpring, useScroll } from "motion/react"
+
+
 
 const App = () => {
+  const { scrollYProgress } = useScroll()
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001,
+    })
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 relative min-h-screen">
-      <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <>
+      <motion.div
+                id="scroll-indicator"
+                style={{
+                    scaleX,
+                    position: "fixed",
+                    zIndex:100,
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    height: 10,
+                    originX: 0,
+                    backgroundColor: "#5D2F8E",
+                }}
+          />
+
+      <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 relative min-h-screen">
+        <div className="fixed top-0 -z-10 h-full w-full">
+          <div className="absolute top-0 z-[-2] h-screen w-screen  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+          </div>
         </div>
+        
+          <div className="container mx-auto px-8"> 
+            <Navbar />
+            <Name />
+            <About/>
+            <Tech/>
+            <Experience/>
+            <Projects/>
+            <Contact/>
+            
+          </div>
       </div>
-      
-        <div className="container mx-auto px-8"> 
-          <Navbar />
-          <Name />
-          <About/>
-          <Tech/>
-          <Experience/>
-          <Projects/>
-          <Contact/>
-          
-        </div>
-    </div>
+    </>
   )
 }
 
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,23 +1,27 @@
 
 import {PROFILE_CONTENT} from '../constants';
 import profilePic from "../assets/myProfilePic.png";
+import { motion } from "motion/react";
 
 
 const Name = () => {
+
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
             <div className="flex flex-wrap">
               <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lgh:text-8xl">Sudharshan <span className= 'bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-6xl tracking-tight text-transparent font-bold'>G</span></h1>
-                    <spam className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</spam>
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{PROFILE_CONTENT}</p>
+                    <motion.h1 
+                      initial={{ opacity: 0, x:-100 }} animate={{ opacity: 1, x:0 }} transition={{duration:0.5, delay:1.5}}
+                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lgh:text-8xl">Sudharshan <span className= 'bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-6xl tracking-tight text-transparent font-bold'>G</span> </motion.h1>
+                    <motion.spam initial={{ opacity: 0, x:-100 }} animate={{ opacity: 1, x:0 }} transition={{duration:0.5, delay:1.6}} className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Full Stack Developer</motion.spam>
+                    <motion.p initial={{ opacity: 0, x:-100 }} animate={{ opacity: 1, x:0 }} transition={{duration:0.5, delay:1.7}} className="my-2 max-w-xl py-6 font-light tracking-tighter">{PROFILE_CONTENT}</motion.p>
                 </div>
               </div>
 
               <div className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex justify-center'>
-                  <img className=' w-100 h-100 lg:h-[465px] rounded-custom lg:ml-16 ' src={profilePic} alt="proopic" />
+                  <motion.img initial={{ opacity: 0, x:100}} animate={{ opacity: 1, x:0 }} transition={{duration: 0.5, delay:1.4, }} className=' w-100 h-100 lg:h-[465px] rounded-custom lg:ml-16 ' src={profilePic} alt="proopic" />
                 </div>
               </div>
             
