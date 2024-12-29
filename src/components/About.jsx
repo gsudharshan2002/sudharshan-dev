ebbbb2ce166d133a7ea33f8072b8ae7d1da7d6d9
G Sudharshan <140174934+gsudharshan2002@users.noreply.github.com>
2024-12-30 00:51:23 +0530

    Update About.jsx
    anime for about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import aboutPic from '../assets/about.jpg';
 import {ABOUT_TEXT} from '../constants';
+import { motion } from "motion/react";
 
 
 
@@ -10,18 +11,21 @@ const About = () => {
         <h1 className="my-20 text-center text-4xl">About 
         <span className="text-neutral-500"> Me</span></h1>
         <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/2 lg:p-8">
+            <motion.div initial={{ opacity: 0, x:-100 }} animate={{ opacity: 1, x:0 }} transition={{duration:0.5, delay:1.5}}
+              className="w-full lg:w-1/2 lg:p-8">
                 <div className="flec items-center justify-center">
                     <img className='rounded-3xl' src={aboutPic} alt="picc" />
                 </div>
-            </div>
+            </motion.div>
             <div className='w-full lg:w-1/2 '>
-                <div style={{display:'inline'}}className='flex flex-warp justify-center lg:justify-start'>
+                <motion.div 
+                  initial={{ opacity: 0, x:100 }} animate={{ opacity: 1, x:0 }} transition={{duration:0.5, delay:1.5}}
+                  style={{display:'inline'}}className='flex flex-warp justify-center lg:justify-start'>
                     <p className='py-8 text-justify '>{ABOUT_TEXT}</p><div onClick={()=>{window.location.href='./'}}className=" text-center w-[200px] px-6 py-3 bg-neutral-900 text-white font-semibold rounded-lg shadow-md cursor-pointer hover:bg-blue-600">
                         View Resume
                     </div>
                     
-                </div>
+                </motion.div>
                     
             </div>
             
